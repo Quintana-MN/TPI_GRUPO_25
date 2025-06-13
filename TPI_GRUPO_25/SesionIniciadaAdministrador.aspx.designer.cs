78bf0c368105cb38a6e6bd4b69e9b0431993e92a
Fede-Giithub <federico.pierdominici@gmail.com>
2025-06-13 14:15:59 -0300

    vistaAdmin

--- TPI_GRUPO_25/SesionIniciadaAdministrador.aspx.designer.cs
+++ TPI_GRUPO_25/SesionIniciadaAdministrador.aspx.designer.cs
@@ -22,5 +22,23 @@ namespace TPI_GRUPO_25
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label usuarioAd;
+
+        /// <summary>
+        /// Control lbl.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
     }
 }

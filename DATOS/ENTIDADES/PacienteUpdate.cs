69f0a5f90c6be5f4cac2c4eefabd6a7d6934420f
Nicolas <128102722+Quintana-MN@users.noreply.github.com>
2025-06-20 22:48:59 -0300

    medico, paciente, persona, usuario updated

---
+++ DATOS/ENTIDADES/PacienteUpdate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTIDADES
+{
+    public class PacienteUpdate
+    {
+        public string nombre;
+        public string email;
+        public string dni;
+
+        public PacienteUpdate(string nombre_Pac, string email_Pac, string dni_Pac)
+        {
+            nombre = nombre_Pac;
+            email = email_Pac;
+            dni=dni_Pac;
+
+        }
+
+    }
+}
---
+++ DATOS/ENTIDADES/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTIDADES
+{
+    public class Usuario
+    {
+        private String contraseniaUsuario;
+        private string nombreUsuario;
+        private bool tipoUsuario;
+
+        public void setContraseniaUsuario(string contrasenia) { contraseniaUsuario = contrasenia; }
+        public string getContraseniaUsuario() { return contraseniaUsuario; }
+
+        public void setNombreUsuario(string nombre) { nombreUsuario = nombre; }
+        public void setTipoUsuario(bool tipo) { tipoUsuario = tipo; }
+
+        public string getNombreUsuario() { return nombreUsuario; }
+        public bool getTipoUsuario() { return tipoUsuario; }
+    }
+}
